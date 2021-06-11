30acecdca7e61b18e94fd81cbb42b540a80b1fda
Yasirzsabri <yasir.zsabri@gmail.com>
2021-06-11 14:16:40 -0600

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'; 
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import { Component } from 'react';
-
+//import logo from './logo.svg';
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'; 
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 class App extends Component{
   render(){
     return(
       <div className="App">
         <header className="App-header">
-          
+          <img  className="App-logo"/>
           <h1 className="App-title">Google Map</h1>
         </header> 
         <Map google={this.props.google} zoom={14}>
