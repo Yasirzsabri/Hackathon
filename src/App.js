49a2329aa5407814248d710e574f4ffb93ef0f07
Yasirzsabri <yasir.zsabri@gmail.com>
2021-06-11 13:58:31 -0600

    logo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
     return(
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          
           <h1 className="App-title">Google Map</h1>
         </header> 
         <Map google={this.props.google} zoom={14}>
@@ -22,6 +22,7 @@ class App extends Component{
             </div>
           </InfoWindow>
         </Map>
+      
 
       </div>
     )
