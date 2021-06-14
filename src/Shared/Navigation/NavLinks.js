d21fe4906a5abccd6b4f0fbf0e9ab6f0b5e62cf8
Abdu <azamgray@gmail.com>
2021-06-13 20:09:34 -0600

    link to button

--- src/Shared/Navigation/NavLinks.js
+++ src/Shared/Navigation/NavLinks.js
@@ -6,7 +6,7 @@ const NavLinks = () => {
     <ul>
       nav links on navLinks page
       <li>
-        <Link to ={{ pathname: "https://www.google.ca/maps/search/atms+near+me" }} target='_blank' >Find Atm Now
+        <Link to ={{ pathname: "https://www.google.ca/maps/search/atms+Calgary,+AB" }} target='_blank' >Find Atm Now
 
         </Link>
       </li>
