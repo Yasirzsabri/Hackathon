284c90b2434b8f7f0c22dc2aeafd1603eee3d6ce
Yasirzsabri <yasir.zsabri@gmail.com>
2021-06-13 20:15:33 -0600

    Co-authored-by: Abdulwahid L Abdulwahid <Abdulwahid2@users.noreply.github.com>

--- src/Shared/Navigation/MainHeader.js
+++ src/Shared/Navigation/MainHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./MainHeader.css";
 const MainHeader = (props) => {
   return <header>
-    main header under props.
+    
     {props.children}</header>;
 };
 
--- src/Shared/Navigation/MainNavigiation.js
+++ src/Shared/Navigation/MainNavigiation.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavLinks from "./NavLinks";
 import MainHeader from "./MainHeader";
 
+
 const MainNavigiation = (props) => {
   return (
     <React.Fragment>
       <MainHeader>
+        
         <nav>
-          nav betweet main header
+          
           <NavLinks />
         </nav>
       </MainHeader>
